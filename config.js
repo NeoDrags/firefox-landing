4a8041224ef18a11cf179dc1362ea815dd30634c
NeoDrags <kesavarapu.prateek@gmail.com>
2021-08-02 11:36:04 +0530

    First commit

---
+++ config.js
@@ -0,0 +1,55 @@
+const ICONS = [
+  {
+    icon: "ri-youtube-fill",
+    link: "https://www.youtube.com/"
+  },
+  {
+    icon: "ri-article-fill",
+    link: "https://docs.google.com/"
+
+  },
+  {
+    icon: "ri-reddit-fill",
+    link: "https://www.reddit.com/"
+  },
+  {
+    icon: "ri-github-fill",
+    link: "https://www.github.com/"
+  },
+  {
+    icon: "ri-book-fill",
+    link: "https://students.allendigital.in/"
+  },
+  {
+    icon: "ri-keyboard-box-fill",
+    link: "https://monkeytype.com"
+  },
+  {
+    icon: "ri-mail-fill",
+    link: "https://mail.google.com"
+  }
+
+]
+
+const LIST_ITEMS = [
+  {
+    name: "r/wallpaper",
+    link: "https://www.reddit.com/r/wallpaper/"
+  },
+  {
+    name: "r/rainmeter",
+    link: "https://www.reddit.com/r/rainmeter/"
+  },
+  {
+    name: "r/unixporn",
+    link: "https://www.reddit.com/r/unixporn/"
+  },
+  {
+    name: "drive",
+    link: "https://drive.google.com"
+  },
+  {
+    name: "translate",
+    link: "https://translate.google.com"
+  }
+]
